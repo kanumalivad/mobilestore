e45335b323a04a7692bf9f24978ff01ef40e5cad
kanumalivad <kanu.malivad@gmail.com>
2019-07-10 00:38:38 +0530

    Initial spring app

---
+++ src/test/java/com/kanu/mobilestore/MobilestoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kanu.mobilestore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MobilestoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
